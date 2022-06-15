5554cf694efe9db7b9c23fea6339b9eb94d99cba
ShinkarenkoMaxim <shinkarenko.max97@gmail.com>
2022-06-16 01:34:32 +0500

    Add dynamic status to successfulResponse notifier

--- src/utils/responseNotifiers.ts
+++ src/utils/responseNotifiers.ts
@@ -1,6 +1,10 @@
 import { ServerResponse } from 'http';
 
-export const successfulResponse = (res: ServerResponse, data: any) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
+export const successfulResponse = (
+  res: ServerResponse,
+  status: number,
+  data?: any
+) => {
+  res.writeHead(status, { 'Content-Type': 'application/json' });
   res.end(data);
 };
